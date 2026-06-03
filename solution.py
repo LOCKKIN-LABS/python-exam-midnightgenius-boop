04001f481372aa1bcffac3f9062135cf36fe7466
mwangieliton-arch <mwangieliton@gmail.com>
2026-06-03 22:04:36 +0300

    Fix AlphaBeta logic

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 def main():
   
-    user_input = float(input()) 
+    user_input = int(input()) 
     
     # --- DO NOT TOUCH THIS LINE ---
     # TODO: Implement core logic here
     # --- DO NOT TOUCH THIS LINE ---
     pass
-
+if user_input % 3==0 and user_input % 5==0:
+    print("AlphaBeta")
+elif user_input % 3==0:
+    print("Alpha")
+elif user_input % 5==0:
+    print("Beta")
+else:
+    print(user_input)
 if __name__ == "__main__":
     main()
