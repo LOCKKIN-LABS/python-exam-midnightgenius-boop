aaf901697cbe3b7504bb6e573dc04fa4438333ea
mwangieliton-arch <mwangieliton@gmail.com>
2026-06-03 22:23:34 +0300

    Fix AlphaBeta logic

--- solution.py
+++ solution.py
@@ -1,18 +1,18 @@
 def main():
   
     user_input = int(input()) 
+    n=int(user_input)
     
     # --- DO NOT TOUCH THIS LINE ---
     # TODO: Implement core logic here
     # --- DO NOT TOUCH THIS LINE ---
     pass
-if user_input % 15 == 0:
-    print("AlphaBeta")
-elif user_input % 3==0:
-    print("Alpha")
-elif user_input % 5==0:
-    print("Beta")
-else:
-    print(user_input)
+if n % 3 == 0:
+    output = "Alpha"
+elif n % 5 == 0:
+    output = "Beta"
+if output == "":
+    output = str(n)  
+print(output)
 if __name__ == "__main__":
     main()
