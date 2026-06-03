9fc36edb5be2f23f83d89dc02ffd5c8afa3a64b7
mwangieliton-arch <mwangieliton@gmail.com>
2026-06-03 22:14:47 +0300

    Fix AlphaBeta Logic

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ def main():
     # TODO: Implement core logic here
     # --- DO NOT TOUCH THIS LINE ---
     pass
-if user_input % 3==0 and user_input % 5==0:
+if user_input % 15 == 0:
     print("AlphaBeta")
 elif user_input % 3==0:
     print("Alpha")
